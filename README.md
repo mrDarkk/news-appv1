facd93038b9d126611d5c0ab4c423da576a0b2b3
Bhupendra Banothe <bhupendrabanothe@gmail.com>
2019-11-19 23:34:28 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# news-app
+
++ Project News App of Android Basics Course.
++ [Android Basics: Networking.](https://classroom.udacity.com/courses/ud843)
+
+## Description 
++ This app displays a list of recent news around the world
++ from the Guardian Free API.
+
+## What I've learned :
++ Using AsyncTasks to connect to the internet.
++ Using AsyncTasksLoader to connect to the internet.
++ Reading and parsing JSON data.
++ Customizing ListViews,using ArrayLists & ArrayAdapters.
++ Using progress bar and listview empty for better UX.
+
+## Things TODO
+
+- [x] The App is Working  :sweat_smile:
+- [ ] Preference Settings  
+- [ ] Better UI
+- [ ] Swipe to refresh
+- [ ] Night Mode
+
+## Screenshots
+
+<img src="https://github.com/mohammed2571994/news-app/blob/master/screenshots/normal.png" width="480" hieght="854">
+<img src="https://github.com/mohammed2571994/news-app/blob/master/screenshots/no_internet.png" width="480" hieght="854">
+
+
+
---
+++ app/src/main/java/com/example/bhupendra/newsapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.bhupendra.newsapp;
+
+
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>>{
+
+    public static final int NEWS_LOADER_ID = 1;
+    public static final String GUARDIAN_REQUEST_URL =
+            " https://content.guardianapis.com/search?api-key=dd5a45a5-28c3-47f7-b5f5-224c71d92bf7";
+
+    ListView mListView;
+    TextView mTextView;
+    NewsAdapter mAdapter;
+    ProgressBar mProgressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        mListView = (ListView) findViewById(R.id.list_view);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mTextView = (TextView) findViewById(R.id.text_view);
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            mProgressBar.setVisibility(View.GONE);
+            mTextView.setVisibility(View.VISIBLE);
+            mTextView.setText(R.string.no_internet_text);
+        }
+    }
+
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int id, Bundle args) {
+        return new NewsLoader(this, GUARDIAN_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
+        mAdapter = new NewsAdapter(this, data);
+        mProgressBar.setVisibility(View.GONE);
+        mListView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+
+    }
+}
